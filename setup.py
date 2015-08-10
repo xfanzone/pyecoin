3d1bb3348dc2850c777994243b212a1617cba166
xfanzone <xfanzone@gmail.com>
2015-08-10 16:13:10 +0800

    fix 0.1 import issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='pyecoin',
-	version='0.1',
+	version='0.2',
 	description='Python wrapper for some e-coin trading sites',
 	author='Feng Xu',
 	author_email='xfanzone@gmail.com',
