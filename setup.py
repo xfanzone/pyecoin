37e8b55ced1c36cb4394328c6a8a8ede3f1b7b42
xfanzone <xfanzone@gmail.com>
2015-08-10 15:44:52 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
-setup(name='pyecoin',
+setup(
+	name='pyecoin',
 	version='0.1',
 	description='Python wrapper for some e-coin trading sites',
 	author='Feng Xu',
 	author_email='xfanzone@gmail.com',
+	url = "https://github.com/xfanzone/pyecoin",
+	download_url = "https://github.com/xfanzone/pyecoin/tarball/0.1",
 	packages=['pyecoin',],
+	keyworks = ['btc', 'ltc', 'btctrade', 'jubi', 'trading']
 	)
