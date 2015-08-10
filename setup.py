3714f9013c54ebbcff35eabafbf64e0ae2a655ad
xfanzone <xfanzone@gmail.com>
2015-08-10 14:38:28 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyecoin',
+	version='0.1',
+	description='Python wrapper for some e-coin trading sites',
+	author='Feng Xu',
+	author_email='xfanzone@gmail.com',
+	packages=['pyecoin',],
+	)
